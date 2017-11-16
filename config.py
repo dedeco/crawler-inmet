4932d6dc341baddeadf1d7803370fe3afb47dd1d
Andre <dedeco@gmail.com>
2017-11-16 12:08:16 -0200

    Up config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+
+DATABASE_URI = 'sqlite:////////Users/dedeco/Projetos/crawler-inmet/clima.db'
+
+USER = 'dedeco@gmail.com'
+PASS = '3ybb3g03'
+
+
+LOGIN_URL = "http://www.inmet.gov.br/projetos/rede/pesquisa/inicio.php"
